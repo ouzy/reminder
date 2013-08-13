c1bad8bbd55492aa9aad9b305c034ccdc2f45d4b
ouzy <ousmane.mariko89@gmail.com>
2013-08-13 18:08:30 -0400

    Removed Reminder class

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -1,3 +1,16 @@
-class Reminder 
+#class Reminder 
 
-end
+
+#end
+
+
+#account_sid = 'ACxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+#auth_token = 'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy'
+
+# set up a client to talk to the Twilio REST API
+#@client = Twilio::REST::Client.new account_sid, auth_token
+#@client.account.sms.messages.create(
+#  :from => '+14159341234',
+#  :to => '+16105557069',
+#  :body => 'Hey there!'
+#)
